7a03d1a914e0b2e0ba66abed7b566e8640ea5dc3
jonashansen95 <jonaserikhansen@gmail.com>
2017-10-03 18:47:08 +0200

    Added Python

---
+++ SensorLoggingRasp.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+import sys
+import Adafruit_DHT
+import requests
+
+url = "https://joerha.dk/climate"
+headers = {"X-API-Key": "d2c9c21f077d65cfe53388a976764472"}
+
+while True:
+
+    humidity, temperature = Adafruit_DHT.read_retry(11, 4)
+
+    payload = {"temp": temperature, "humidity": humidity}
+
+    r = requests.post(url, headers=headers, data=payload)
+    print r.status_code
